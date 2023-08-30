86212985c91c0248c792658997e707f28ebd2aa0
saurav-saha <sauravsaha024@gmail.com>
2023-08-30 07:57:16 +0530

    con

--- script.js
+++ script.js
@@ -6,22 +6,38 @@ const navel = document.querySelector(".navbar");
 const slides = document.querySelectorAll(".projects");
 const prevBtn = document.querySelector(".leftI");
 const nextBtn = document.querySelector(".rightI");
+const i1EL = document.querySelector(".i1");
+const detailsEL = document.querySelector(".details");
+
 let slideIndex = 0; // take a index number
 
+// WHEN WEBSITE IS APPER AT FRIST**********
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    i1EL.style.transform = "scale(1)"; //SLIDE IMAGE SCALE WILL BE 1 after 1 secound
+  }, 1000);
+});
+// Responsive*********
 BARel.addEventListener("click", () => {
   navel.classList.toggle("slide"); // SLIDE TOGGLE MEANS SLIDE COME AND GOSE ON SAME BUTTTON
   BARel.classList.toggle("wcolor"); // COLOR WHITE CLICK THE BUTTTON
 });
-// ADD ACTIVE OR REMOVE ACTIVE
+
+// ADD ACTIVE OR REMOVE ACTIVE*************
 function showSlide(index) {
   slides.forEach((slide) => {
     //USING FOR EACH LOOP *** FRIST REMOVE THE ACTIVE
     slide.classList.remove("active");
     slide.classList.remove("scale"); // REMOVE SCALE
+    // slide.classList.remove("activeTex"); // REMOVE SCALE
   });
   slides[index].classList.add("active");
   slides[index].classList.add("scale"); // ACTIVE SCALE **NEED EXTRA DIV NOT WORKING ON SAME DIV
 }
+setInterval(() => {
+  slides[index].detailsEL.classList.remove("removeTex");
+}, 3000);
 
 // THIS FUNCTION BACKWARD THE SLIDE
 function prevSlide() {
@@ -40,6 +56,7 @@ function nextSlide() {
     // IF INDEX NUMBER  EQUAL TO TOTAL NUMBER OF SLIDE
     slideIndex = 0;
   }
+
   showSlide(slideIndex);
 }
 // ABOVE FUNCTION ARE CALL IF CLICK THISE BUTTON
